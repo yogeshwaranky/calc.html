4ef9d736f397a122b431850e2841aa298622d733
yogeshwaranky <yogeshwaranky2103@email.com>
2023-07-23 19:06:59 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,13 @@
+function display(val){
+  document.getElementById('result').value += val
+  return val
+}
+function solve(){
+  let x = document.getElementById('result').value
+  let y = eval(x);
+  document.getElementById('result').value= y
+  return y
+}
+function clearscreen(){
+  document.getElementById('result').value= ''
+}
